bb364dea4c5e4f4437b2969faeef2562c9fc300e
newming <a977527479@sina.com>
2016-08-03 15:32:13 +0800

    add two router

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,11 +7,16 @@ class Home extends React.Component {
         width:'100vw',
         height:'100vh',
         background: 'url("http://7xopqp.com1.z0.glb.clouddn.com/newming24.jpg") no-repeat fixed center center/cover'
+      },
+      title:{
+        color:'#fff',
+        textAlign:'center',
+        paddingTop:'30%'
       }
     }
     return(
       <div style={styles.root}>
-
+        <h1 style={styles.title}>欢迎光临</h1>
       </div>
     )
   }
--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,13 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './App.js';
 import Home from './components/Home.js';
+import About from './components/About.js';
+import Blog from './components/Blog.js';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Home} />
+    <Route path="blog" component={Blog} />
+    <Route path="about" component={About} />
   </Route>
 )
