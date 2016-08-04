e88fda5eafd7375de8982d639964361214db5109
newming <a977527479@sina.com>
2016-08-04 15:08:43 +0800

    test

--- src/utils/helpers.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-function getUserInfo(username){
-  return axios.get(`https://api.github.com/users/${username}`)
-    .then((res) => ({
-      bio: res.data,
-    }))
-}
-export {getUserInfo};
